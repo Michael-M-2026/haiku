3db61541195c879cd48f81b86f7d6f796c58059f
Michael-M-2026 <mmabbun22@gmail.com>
2026-03-31 20:20:03 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ function randomize() {
   grid[0].style.transform = 'rotate(${Math.random() * 360}deg';
 
   // just the grid divs
+  divs.forEach(function (div) {
   let scale = randomNumber (.5, 1.2);
   let translateX = randomNumber (0, 50);
   let translateY = randomNumber (0, 20);
@@ -21,6 +22,7 @@ function randomize() {
   }
 
   div.style.transform ='scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)'; 
+});
 }
 
 function randomize() {
